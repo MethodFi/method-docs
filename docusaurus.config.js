5f41966ec319ccc3d8fbaf4c032c5e018625e707
Marco del Carmen <marco@mdelcarmen.me>
2021-10-22 16:16:25 -0700

    Add license and updated editUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -26,7 +26,7 @@ const config = {
         docs: {
           routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.js'),
-          editUrl: 'https://github.com/MethodFi/method-docs',
+          editUrl: 'https://github.com/MethodFi/method-docs/tree/release',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
