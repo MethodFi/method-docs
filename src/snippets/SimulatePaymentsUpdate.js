81ba94968aa2fa3671cae39d5cec2906e91d4c16
Marco del Carmen <marco@mdelcarmen.me>
2022-02-28 20:26:43 -0600

    Add docs for pagination, filtering, and simulations.

---
+++ src/snippets/SimulatePaymentsUpdate.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import TabbedCodeBlock from '../components/TabbedCodeBlock';
+
+const curl = `
+curl https://dev.methodfi.com/simulate/payments/pmt_rPrDPEwyCVUcm \\
+  -X POST \\
+  -H "Authorization: Bearer sk_WyZEWVfTcH7GqmPzUPk65Vjc" \\
+  -H "Content-Type: application/json" \\
+  -d '{
+    "status": "processing"
+  }'
+`.trim();
+
+const nodejs = `
+const payment = await method.simulate.payments.update('pmt_rPrDPEwyCVUcm', { status: 'processing' });
+`.trim();
+
+const python = `
+payment = method.simulate.payments.update('pmt_rPrDPEwyCVUcm', { 'status': 'processing' })
+`.trim();
+
+export default function () {
+  return (
+    <TabbedCodeBlock
+      groupId="all"
+      name="simulate-payments-update"
+      items={[
+        { title: 'cURL', language: 'shell', content: curl },
+        { title: 'Node.js', language: 'javascript', content: nodejs },
+        { title: 'Python', language: 'python', content: python },
+      ]} />
+  );
+}
