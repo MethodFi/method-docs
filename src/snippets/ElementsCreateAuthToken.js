a0ea5a745c4ca313ad7468538446558bfa2f6e0d
Marco del Carmen <marco@mdelcarmen.me>
2022-04-18 17:24:15 -0500

    Auth Demo

---
+++ src/snippets/ElementsCreateAuthToken.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import TabbedCodeBlock from '../components/TabbedCodeBlock';
+
+const curl = `
+curl https://production.methodfi.com/elements/token \\
+  -X POST \\
+  -H "Authorization: Bearer sk_WyZEWVfTcH7GqmPzUPk65Vjc" \\
+  -H "Content-Type: application/json" \\
+  -d '{
+    "entity_id": "ent_au22b1fbFJbp8",
+    "team_name": "Demo Auth App",
+    "type": "auth",
+    "auth": {}
+  }'
+`.trim();
+
+const nodejs = `
+const token = await method.elements.createToken({
+  entity_id: 'ent_au22b1fbFJbp8',
+  team_name: 'Demo Auth App',
+  type: 'auth',
+  auth: {},
+});
+`.trim();
+
+const python = `
+token = method.elements.create_token({
+  'entity_id': 'ent_au22b1fbFJbp8',
+  'team_name': 'Demo Auth App',
+  'type': 'auth',
+  'auth': {}
+})
+`.trim();
+
+export default function () {
+  return (
+    <TabbedCodeBlock
+      groupId="all"
+      name="elements-create-auth-token"
+      items={[
+        { title: 'cURL', language: 'shell', content: curl },
+        { title: 'Node.js', language: 'javascript', content: nodejs },
+        { title: 'Python', language: 'python', content: python },
+      ]} />
+  );
+}
