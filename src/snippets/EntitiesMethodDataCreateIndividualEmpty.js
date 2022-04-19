677003910a78ebd3fbb87cef419e0007b4bfea6d
Marco del Carmen <marco@mdelcarmen.me>
2022-04-18 19:39:00 -0500

    Default to Auth Element for quick start guide.

---
+++ src/snippets/EntitiesMethodDataCreateIndividualEmpty.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import TabbedCodeBlock from '../components/TabbedCodeBlock';
+
+const curl = `
+curl https://production.methodfi.com/entities \\
+  -X POST \\
+  -H "Authorization: Bearer sk_WyZEWVfTcH7GqmPzUPk65Vjc" \\
+  -H "Content-Type: application/json" \\
+  -d '{
+    "type": "individual",
+    "individual": {}
+  }'
+`.trim();
+
+const nodejs = `
+const entity = await method.entities.create({
+  type: 'individual',
+  individual: {},
+});
+`.trim();
+
+const python = `
+entity = method.entities.create({
+  'type': 'individual',
+  'individual': {}
+})
+`.trim();
+
+export default function () {
+  return (
+    <TabbedCodeBlock
+      groupId="all"
+      name="create-method-data-individual-empty"
+      items={[
+        { title: 'cURL', language: 'shell', content: curl },
+        { title: 'Node.js', language: 'javascript', content: nodejs },
+        { title: 'Python', language: 'python', content: python },
+      ]} />
+  );
+}
