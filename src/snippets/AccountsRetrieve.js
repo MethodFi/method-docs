fda15dc5e270975a92584fe9329688f27c25a329
mustafalii <mustafaali.py@gmail.com>
2022-09-12 16:22:23 -0500

    AccountsRetrieve

--- src/snippets/AccountsRetrieve.js
+++ src/snippets/AccountsRetrieve.js
@@ -14,7 +14,42 @@ const python = `
 account = method.accounts.get('acc_Zc4F2aTLt8CBt')
 `.trim();
 
-const response = {};
+const response = {
+  "id": "acc_Zc4F2aTLt8CBt",
+  "holder_id": "ent_au22b1fbFJbp8",
+  "type": "liability",
+  "ach": null,
+  "liability": {
+    "mch_id": "mch_2",
+    "mask": "4455",
+    "type": "student_loan",
+    "data_status": "pending",
+    "data_last_successful_sync": null,
+    "student_loan": {
+      "name": "FedLoan Student Loan",
+      "sub_type": "federal",
+      "sequence": 6,
+      "balance": 1952634,
+      "last_payment_amount": 1150,
+      "last_payment_date": "2022-04-22",
+      "next_payment_due_date": "2022-10-28",
+      "next_payment_minimum_amount": 0,
+      "disbursed_at": "2018-01-22",
+      "interest_rate_percentage": 3.4,
+      "interest_rate_type": "fixed"
+    }
+  },
+  "clearing": null,
+  "metadata": null,
+  "status": "active",
+  "capabilities": [
+    "payments:receive",
+    "data:sync"
+  ],
+  "error": null,
+  "created_at": "2020-12-09T02:56:19.548Z",
+  "updated_at": "2020-12-09T02:56:19.548Z"
+};
 
 export default function () {
   return (
